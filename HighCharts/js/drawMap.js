75db4ba872a4556d226c629b6de1856b3da2a0d4
Andrew McOlash <amcolash@gmail.com>
2014-12-22 16:37:27 -0600

    Added a graph to highcharts test, colored based on 'section'

--- HighCharts/js/drawMap.js
+++ HighCharts/js/drawMap.js
@@ -294,21 +294,34 @@ $(function () {
 
   var average = 0;
 
+  var color = [
+   "#AA3C39",
+   "#AA6D39",
+   "#236467",
+   "#2D8632",
+   "#AA8E39"
+  ];
+
   // Print out a list of this data
   $.each(Highcharts.maps["countries/us/us-wi-all"].features, function(i, item) {
     // Make the data random and fun :)
     data[i].value = Math.floor((Math.random() * 1000) + 1);
+    data[i].y = data[i].value;
+    data[i].section = Math.floor(i/(data.length/5));
+    data[i].borderColor = color[data[i].section];
+    data[i].name = item.properties.name;
+
     average += data[i].value;
     console.log("id: " + item.id + ", name: " + item.properties.name);
-    $('#myTable tr:last').after('<tr><td>' + i + '</td><td>' + item.properties.name + '</td><td>' + data[i].value +
-        '</td><td>' + item.id + '</td></tr>');
+    $('#myTable tr:last').after('<tr><td>' + i + '</td><td>' + item.properties.name + '</td><td>' + data[i].section 
+        + '</td><td>' + data[i].value + '</td><td>' + item.id + '</td></tr>');
   });
 
   average = Math.floor(average / data.length);
   $('#avg').text(average);
 
   // Initiate the chart
-  $('#map').highcharts('Map', {
+  $('#map1').highcharts('Map', {
 
     title : {
       text : 'Highmaps basic demo'
@@ -328,7 +341,7 @@ $(function () {
     */
 
     chart: {
-      backgroundColor: null
+      backgroundColor: null,
     },
 
     colorAxis: {
@@ -349,7 +362,49 @@ $(function () {
       dataLabels: {
         enabled: false,
         format: '{point.name}'
+      },
+      borderWidth: 2
+    }]
+  });
+
+  $('#chart1').highcharts({
+    chart: {
+      type: 'column',
+      backgroundColor: null
+    },
+    title: {
+      text: 'Random Wisconsin Data'
+    },
+    subtitle: {
+      text: 'Source: Random'
+    },
+    xAxis: {
+      title: {
+        text: 'Index Number'
       }
+    },
+    yAxis: {
+      min: 0,
+      max: 1000,
+      title: {
+        text: 'Random Data'
+      }
+    },
+    /*
+    plotOptions: {
+      column: {
+        pointPadding: 0.2,
+        borderWidth: 0
+      }
+    },
+    */
+    series : [{
+      data : data,
+      dataLabels: {
+        enabled: true,
+        format: '{point.name}'
+      },
+      shadow: true
     }]
   });
   
