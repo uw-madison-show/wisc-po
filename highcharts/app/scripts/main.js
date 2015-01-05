a2dcb54e386425ac41deaf31cf8bcbf5ee43f793
Andrew McOlash <amcolash@gmail.com>
2015-01-05 15:16:34 -0600

    Updated to now using highcharts instead of highmaps

--- highcharts/app/scripts/main.js
+++ highcharts/app/scripts/main.js
@@ -132,6 +132,55 @@ $('#chart1').highcharts({
   }]
 });
 
+$('#line1').highcharts({
+  chart: {
+    zoomType: 'xy'
+  },
+  title: {
+    text: 'Temperature'
+  },
+  xAxis: [{
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  }],
+  yAxis: [{ // Primary yAxis
+    labels: {
+      formatter: function() {
+        return this.value + '°C';
+      },
+      style: {
+        color: '#89A54E'
+      }
+    },
+    title: {
+      text: 'Temperature',
+      style: {
+        color: '#89A54E'
+      }
+    }
+  }],
+
+  tooltip: {
+    shared: true
+  },
+
+  series: [{
+    name: 'Temperature',
+    color: '#89A54E',
+    type: 'spline',
+    data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
+    tooltip: {
+      pointFormat: '<span style="font-weight: bold; color: {series.color}">{series.name}</span>: <b>{point.y:.1f}°C</b> '
+    }
+  }, {
+    name: 'Temperature error',
+    type: 'errorbar',
+    data: [[6, 8], [5.9, 7.6], [9.4, 10.4], [14.1, 15.9], [18.0, 20.1], [21.0, 24.0], [23.2, 25.3], [26.1, 27.8], [23.2, 23.9], [18.0, 21.1], [12.9, 14.0], [7.6, 10.0]],
+    tooltip: {
+      pointFormat: '(error range: {point.low}-{point.high}°C)<br/>'
+    }
+  }]
+});
+
 function randomData() {
   console.log("randomness!");
   for (var i = 0; i < data.length; i++) {
