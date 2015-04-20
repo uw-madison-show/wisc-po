290a14113f762f9eb5e2b9dba48ecb701e7578c8
Andrew McOlash <amcolash@gmail.com>
2015-04-20 13:31:28 -0500

    Made all stable things no longer prefix (for cacheing)

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -233,9 +233,9 @@ module.exports = function (grunt) {
           src: [
             '<%= config.dist %>/scripts/{,*/}*.js',
             '<%= config.dist %>/styles/{,*/}*.css',
-            '<%= config.dist %>/images/{,*/}*.*',
-            '<%= config.dist %>/styles/fonts/{,*/}*.*',
-            '<%= config.dist %>/*.{ico,png}'
+            // '<%= config.dist %>/images/{,*/}*.*',
+            // '<%= config.dist %>/styles/fonts/{,*/}*.*',
+            // '<%= config.dist %>/*.{ico,png}'
           ]
         }
       }
